369b2a82a4b23fa0fcd92f724b27c33b26994f0f
Andrew H <aho338@gmail.com>
2016-04-07 10:25:54 -0700

    Initialize ViewContainer and ViewEntry components

---
+++ client/components/ViewEntry.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ViewEntry extends React.Component {
+  render() {
+    return <h2>This is a view</h2>
+  }
+}
+
+ReactDOM.render(
+  <ViewEntry/>,
+  document.getElementById('view-entry')
+);
