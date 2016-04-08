42409da5f2bc41b4815e9bbcad47dffb8047061e
Joel Aguero <joelmaguero@gmail.com>
2016-04-08 15:02:03 -0700

    Refactor using Redux connect with props mapped

--- client/components/App.js
+++ client/components/App.js
@@ -1,23 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import updateViews from '../actions';
 
 import Search from './Search';
 import ViewContainer from './ViewContainer';
 
+const mapStateToProps = (state) => {
+  return {
+    views: state.views
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onEnter: (loc) => {
+      dispatch(updateViews(loc));
+    }
+  };
+};
+
+App.propTypes = {
+  views: PropTypes.array.isRequired,
+  onEnter: PropTypes.func.isRequired
+};
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      viewEntries: ['View 1', 'View 2', 'View 3']
-    };
-  }
   render() {
+    const { views, onEnter } = this.props;
     return (
       <div>
-        <Search/>
-        <ViewContainer viewEntries={this.state.viewEntries} />
+        <Search onEnter={ (loc) => onEnter(loc) } />
+        <ViewContainer viewEntries={views} />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
