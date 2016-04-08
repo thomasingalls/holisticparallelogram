11b010007cd361fdc58d5c094afafca098dec630
Joel Aguero <joelmaguero@gmail.com>
2016-04-08 15:05:21 -0700

    Make styling tweaks

--- client/components/App.js
+++ client/components/App.js
@@ -5,6 +5,18 @@ import updateViews from '../actions';
 import Search from './Search';
 import ViewContainer from './ViewContainer';
 
+class App extends React.Component {
+  render() {
+    const { views, onEnter } = this.props;
+    return (
+      <div>
+        <Search onEnter={ (loc) => onEnter(loc) } />
+        <ViewContainer viewEntries={views} />
+      </div>
+    );
+  }
+}
+
 const mapStateToProps = (state) => {
   return {
     views: state.views
@@ -23,18 +35,6 @@ App.propTypes = {
   views: PropTypes.array.isRequired,
   onEnter: PropTypes.func.isRequired
 };
-
-class App extends React.Component {
-  render() {
-    const { views, onEnter } = this.props;
-    return (
-      <div>
-        <Search onEnter={ (loc) => onEnter(loc) } />
-        <ViewContainer viewEntries={views} />
-      </div>
-    );
-  }
-}
 
 export default connect(
   mapStateToProps,
