30eca636e5028d67f0787c6245869b0073b54ca0
Reina Mei <reinameic@gmail.com>
2016-04-08 15:01:46 -0700

    Bug fixes to apiController.js

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -4,60 +4,82 @@
 
 var GOOGLE_PLACES_API_KEY = require(__dirname + '/../config/googleplaces.js');
 var request = require('request');
+// var bodyParser = require('body-parser');
+
 var urlParser = require('url');
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 //TODO: require body-parser in server.js
 
 
 //Make a get call to Google Places nearbysearch, get back 20 results;
 //Make a get call to Google Places details for each of the 20 results, match their reviews against a regex, send filtered and simplified results back to client;
-//Use a counter to make sure the results are only sent to client after all the initial results have been examined.
+//Use a counter to make sure the results are only sent to client after all the initial results have been examined;
+//Callback depths are labeled by layers;
 module.exports.getAll = function(req, res) { //only returns 20 results per call, needs to pass in pagetoken returned from previous call in order to get the next 20 results
+
   // var next_page_token;
   var searchString = urlParser.parse(req.url).search; //includes leading question mark
-  var regex = new RegExp(/(good|great|awesome|fantastic|terrific|nice|cool|wonderful|dope|beautiful|amazing|gorgeous|breathtaking) view/, 'i');
+  var regex = new RegExp(/(good|great|awesome|fantastic|terrific|nice|cool|wonderful|dope|beautiful|amazing|gorgeous|breathtaking) view/);
+  console.log(searchString);
 
   request.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json' + searchString + '&key=' + GOOGLE_PLACES_API_KEY)
-    .on('response', function(response) {
-      console.log(response.body.results);
-      var filteredBody = {};
-      filteredBody.places = [];
-      var places = response.body.results;
-      var counter = 0; //ensures server only sends back filteredBody if all places have been processed
-      for (var i = 0; i < places.length; i++) {
-        var place = places[i];
-        var placeid = place['place_id'];
+    .on('response', function(response) { //layer 1 on 'response'
 
-        request.get('https://maps.googleapis.com/maps/api/place/details/json?' + 'key=' + GOOGLE_PLACES_API_KEY + '&placeid=' + placeid)
-          .on('response', function(response) {
-            var placeDetails = response.body.result;
-            var reviews = placeDetails.reviews;
-            for (var j = 0; j < reviews.length; j++) {
-              var review = reviews[j];
-              if (review.text.match(regex)) { //TODO: improve regex matching
-                filteredBody.places.push({
-                  name: placeDetails.name,
-                  address: placeDetails['formatted_address']
-                });
-                break;
-              }
-            }
-            counter++;
-            if (counter === places.length) {
-              res.json(filteredBody); //also needs to pass page token back to client for further requests
-            } //TODO: move it inside callback above
-          })
-          .on('error', function(error) {
-            //TODO: handle error
-            counter++;
-            if (counter === places.length) {
-              res.json(filteredBody); //also needs to pass page token back to client for further requests
-            } 
-          })
+      var body = [];
 
-    })
-    .on('error', function(error) {
+      response.on('data', function(chunk) { //layer 2 on 'data'
+        body.push(chunk);
+      }).on('end', function() { //layer 2 on 'end'
+        body = JSON.parse(Buffer.concat(body).toString());
+        var filteredBody = {};
+        filteredBody.places = [];
+        var places = body.results;
+        var counter = 0; //ensure server only sends back filteredBody if all places have been processed
+        for (var i = 0; i < places.length; i++) {
+          var place = places[i];
+          var placeid = place['place_id'];
+
+          request.get('https://maps.googleapis.com/maps/api/place/details/json?' + 'key=' + GOOGLE_PLACES_API_KEY + '&placeid=' + placeid)
+            .on('response', function(response) { //layer 3 on 'response'
+              var body = [];
+              response.on('data', function(chunk) { //layer 4 on 'data'
+                body.push(chunk);
+              }).on('end', function() { //layer 4 on 'end'
+                body = JSON.parse(Buffer.concat(body).toString());
+                var placeDetails = body.result;
+                var reviews = placeDetails.reviews;
+                if (reviews) {
+                  for (var j = 0; j < reviews.length; j++) {
+                    var review = reviews[j];
+                    if (review.text.match(regex)) { //TODO: improve regex matching
+                      filteredBody.places.push({
+                        name: placeDetails.name,
+                        address: placeDetails['formatted_address']
+                      });
+                      break;
+                    }
+                  }
+                }
+                counter++;
+                if (counter === places.length) {
+                  res.json(filteredBody); //also needs to pass page token back to client for further requests
+                }
+              }); //end of layer 4 on 'end'
+            }) //end of layer 3 on 'response'
+            .on('error', function(error) { //layer 3 on 'error'
+              //TODO: handle error
+              counter++;
+              if (counter === places.length) {
+                res.json(filteredBody); //also needs to pass page token back to client for further requests
+              } 
+            }) //end of layer 3 on 'error'
+        }
+      }); //end of layer 2 on 'end'
+    }) //end of layer 1 on 'response'
+    .on('error', function(error) { //layeon 'error'
       //TODO: handle error
-    });
+    }); //end of layer 1 on 'error'
 };
 
 
