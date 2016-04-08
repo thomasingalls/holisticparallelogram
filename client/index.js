843cb92b1d27289637fae7414eccdb4ac8dc27d5
Joel Aguero <joelmaguero@gmail.com>
2016-04-08 15:04:20 -0700

    Make minor styling tweaks

--- client/index.js
+++ client/index.js
@@ -4,8 +4,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import zip from './reducers';
 
 import App from './components/App.js';
