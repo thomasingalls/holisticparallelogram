9a160d08d844e34e983ef6c6c22d002c9a6b2637
Joel Aguero <joelmaguero@gmail.com>
2016-04-11 15:05:16 -0700

    Display heart with place entries

--- client/components/PlaceEntry.js
+++ client/components/PlaceEntry.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import classNames from 'classnames';
+
+var placeEntryClassNames = classNames('grid', 'place-entry');
 
 var PlaceEntry = (props) => (
-  <div>
-    <h3>{ props.place.name }</h3>
-    <p>{ props.place.address }</p>
+  <div className={placeEntryClassNames}>
+    <div className='col-2-8'>
+      <span className="icon-heart" aria-hidden="true"></span>
+    </div>
+    <div className='col-6-8'>
+      <h3>{ props.place.name }</h3>
+      <p>{ props.place.address }</p>
+    </div>
   </div>
 );
 
