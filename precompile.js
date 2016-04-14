959e808e4cfc5389f85a840bb32187cb6dabb87f
Andrew H <aho338@gmail.com>
2016-04-14 11:37:24 -0700

    Add documentation to precompile.js

--- precompile.js
+++ precompile.js
@@ -1,2 +1,11 @@
+/*
+This file allows ES2015 syntax to be used on the server-side, and serves as the
+entry point for the server code. Babel compiles server.js dependencies to ES5.
+ES2015 is used on the server for initializing the Redux state and sending the
+index page to the client.
+
+https://babeljs.io/docs/usage/require/
+*/
+
 require('babel-register');
 var scenicApp = require('./server.js');
