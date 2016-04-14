d5716279c6873245646736f8d787ef62a1431d29
Reina Mei <reinameic@gmail.com>
2016-04-14 15:50:28 -0700

    Fix bug where every place comes back with same id

--- server/places/placeController.js
+++ server/places/placeController.js
@@ -29,10 +29,10 @@ module.exports.saveOne = function(req, res) {
   })
   .then(function(foundUser) {
     Place.findOrCreate({where: place})
-    .spread(function(createdPlace) {
-      foundUser.addPlace(createdPlace)
+    .spread(function(foundOrCreatedPlace) {
+      foundUser.addPlace(foundOrCreatedPlace)
       .then(function() {
-        res.json(createdPlace);
+        res.json(foundOrCreatedPlace);
       });
     });
   });
@@ -40,23 +40,47 @@ module.exports.saveOne = function(req, res) {
 
 module.exports.deleteOne = function(req, res) {
   var user = req.body.user;
-
-  // the client will pass in a place obj w/ googlePlaceId, name, address
   var place = req.body.place;
 
-  // find the id of the place given the googlePlaceId
-  Place.findOne({ 
-    where: {googlePlaceId: place.googlePlaceId} 
+  User.findOne({
+    where: user
   })
-  .then(function(place) {
-    // remove the association between the user and the place
-    user.removePlace(place);
-      // TODO: For future, do a check: 
-      // if no users have a place with the same id as this one,
-      // delete that place from the places table so that you don't end
-      // up with lots of places that aren't associated with any users.
-      // This will only matter if this app goes global!
+  .then(function(foundUser) {
+    // Place.findOne({
+    //   where: place
+    // })
+    // .then(function(foundPlace) {
+    //   res.json(foundPlace);
+    // });
+    res.json(user);
+
   });
+
+  // User.findOne({
+  //   where: user
+  // })
+  // .then(function(foundUser) {
+  //   Place.findOne({where: place})
+  //   .then(function(foundPlace) {
+  //     // foundUser.removePlace(foundPlace)
+  //     // .then(function() {
+  //       res.json(foundPlace);
+  //     // });
+  //   });
+  // });
+
+  // Place.findOne({ 
+  //   where: {googlePlaceId: place.googlePlaceId} 
+  // })
+  // .then(function(place) {
+  //   // remove the association between the user and the place
+  //   user.removePlace(place);
+  //     // TODO: For future, do a check: 
+  //     // if no users have a place with the same id as this one,
+  //     // delete that place from the places table so that you don't end
+  //     // up with lots of places that aren't associated with any users.
+  //     // This will only matter if this app goes global!
+  // });
 };
 
 
@@ -104,7 +128,7 @@ module.exports.searchGoogle = function(req, res) {
                         filteredBody.places.push({
                           name: placeDetails.name,
                           address: placeDetails['formatted_address'],
-                          googlePlaceId: placeid
+                          googlePlaceId: placeDetails['place_id']
                         });
                         break;
                       }
