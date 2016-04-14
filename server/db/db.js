79727cf286a4d9ac59c83ea404ece86d02be8727
Laura Curley <info@lauracurley.com>
2016-04-14 11:55:17 -0700

    Update db config files for production deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 var Sequelize = require('sequelize');
+var password = require('../config/mysqlsetup.js');
 
-var db = new Sequelize('scenic', 'root', '', {
+// Using ES2015:
+// import password from '../config/mysqlsetup.js';
+
+var db = new Sequelize('scenic', 'root', password, {
   define: {
     timestamps: false // true by default
   }
