10b9fdbea3c3144c0b768613c4a55ac72d24b8f0
Reina Mei <reinameic@gmail.com>
2016-04-18 11:39:50 -0700

    Show a user's saved views immediately after login

--- server/router/indexHandler.js
+++ server/router/indexHandler.js
@@ -5,38 +5,59 @@ import { Provider } from 'react-redux';
 import rootReducer from '../../client/reducers/index';
 import App from '../../client/components/App';
 import renderFullPage from '../views/index';
+var User = require(__dirname + '/../users/userModel.js');
+
 
 // Handler for rendering the index page with user data, if available
 // http://redux.js.org/docs/recipes/ServerRendering.html
 module.exports = function(req, res) {
   // Initialize user state if user is logged in
   var user = {};
+  var savedPlaces = [];
+
+  var sendInitialState = function() {
+    // Create a new Redux store instance
+    const store = createStore(rootReducer, {
+      places: [],
+      savedPlaces: savedPlaces,
+      user: user
+    });
+
+    // Render the component to a string
+    const html = renderToString(
+      <Provider store={store}>
+      <App />
+      </Provider>
+    );
+
+    // Grab the initial state from our Redux store
+    const initialState = store.getState();
+
+    // Send the rendered page back to the client as a String
+    res.send(renderFullPage(html, initialState));
+
+  };
+
   if (req.session.passport && req.session.passport.user) {
     user = {
       googleUserId: req.session.passport.user.id,
       firstName: req.session.passport.user.name.givenName || null,
       lastName: req.session.passport.user.name.familyName || null
       // avatarUrl: req.session.passport.user.photos[0].value || null,
-    }
+    };
+
+    User.findOne({
+        where: user
+      })
+      .then(function(foundUser) {
+        return foundUser.getPlaces();
+      })
+      .then(function(foundPlaces) {
+        savedPlaces = foundPlaces;
+        sendInitialState();
+      });
+  } else {
+    sendInitialState();
   }
 
-  // Create a new Redux store instance
-  const store = createStore(rootReducer, {
-    places: [],
-    savedPlaces: [],
-    user: user
-  });
-
-  // Render the component to a string
-  const html = renderToString(
-    <Provider store={store}>
-    <App />
-    </Provider>
-  );
-
-  // Grab the initial state from our Redux store
-  const initialState = store.getState();
-
-  // Send the rendered page back to the client as a String
-  res.send(renderFullPage(html, initialState));
 }
