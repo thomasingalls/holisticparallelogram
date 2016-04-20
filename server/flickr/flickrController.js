9a8808bed6a65fca386aade4f71f6841c590f3a2
Jonathen Chen <jonathen0915@gmail.com>
2016-04-20 16:35:42 -0700

    Fix merge where API keys where showing

--- server/flickr/flickrController.js
+++ server/flickr/flickrController.js
@@ -8,9 +8,27 @@ var urlParser = require('url');
 module.exports.searchFlickr = function(req, res) {
   //search coordinates or string
   var queryString = urlParser.parse(req.url).search;
-  var queryCoord = urlParser.parse(req.url).coord;
+  var lon = urlParser.parse(req.url).lon;
+  var lat = urlParser.parse(req.url).lat;
+  var radius = 1;
+  var accuracy = 11
   var method ='flickr.photos.search';
-  // https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key
+  var sort = 'relevance'
+  //The possible values are: date-posted-asc, date-posted-desc, date-taken-asc, date-taken-desc, interestingness-desc, interestingness-asc, and relevance.
 
-  request.get('https://api.flickr.com/services/rest/?method=' + method +'&text=' + queryString +  '&api_key=' + FLICKR_API_KEY.KEY)
+  request.get('https://api.flickr.com/services/rest/?method=' + method +'&text=' + queryString + '&accuracy=' + accuracy +'&lat=' + lat + '&lon=' + lon + '&radius=' + radius + '&sort=' + sort + '&api_key=' + FLICKR_API_KEY.KEY)
+}
+
+module.exports.getPhotoURl = function(photoID, size) {  //return image source
+  request.get('https;//api.flickr.com/services/rest/?method=flickr.photos.getSizes&photo_id=' + photoID + '&api_key=' + FLICKR_API_KEY.KEY)
+          .on('response', function(response) {
+            var body = [];
+            response.on('data', function(chunk) {
+              body.push(chunk);
+            }).on ('end', function() { //select the right image and return the data
+              //go through the entire list
+              //select the right size
+              return body.something[index].source;
+            })
+          })
 }
