3864c44d236a0c803232d825ef601e10aac48006
Jonathen Chen <jonathen0915@gmail.com>
2016-04-20 15:24:03 -0700

    Update flickr API to pull based on text query

--- server/flickr/flickrController.js
+++ server/flickr/flickrController.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var flickrImage = require(__dirname + '/placeModel.js');
 var User = require(__dirname + '/../users/userModel.js');
-var FLICKR_API_KEY = require(__dirname + '/../config/flickrAPI.js');
+var FLICKR_API_KEY = require(__dirname + '/../config/flickr.js');
 var request = require('request');
 var urlParser = require('url');
 
@@ -9,6 +9,8 @@ module.exports.searchFlickr = function(req, res) {
   //search coordinates or string
   var queryString = urlParser.parse(req.url).search;
   var queryCoord = urlParser.parse(req.url).coord;
+  var method ='flickr.photos.search';
+  // https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=e473927df4ee4e286fe09119cadfd471
 
-  request.get('')
+  request.get('https://api.flickr.com/services/rest/?method=' + method +'&text=' + queryString +  '&api_key=' + FLICKR_API_KEY.KEY)
 }
