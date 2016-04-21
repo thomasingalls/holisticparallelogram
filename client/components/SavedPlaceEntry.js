4ef759b7f10990e000af3d556093eade8ce6e302
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-04-21 14:15:07 -0700

    Refactor saved place entry to extend react components

--- client/components/SavedPlaceEntry.js
+++ client/components/SavedPlaceEntry.js
@@ -1,17 +1,54 @@
-import React from 'react';
+// OLD WORKING CODE
+// import React from 'react';
 
-var SavedPlaceEntry = (props) => (
-  <div className='saved-place-entry animated fadeIn'>
-    <p className='saved-place-name'>{ props.savedPlace.name }</p>
-    <p className='saved-place-address'>{ props.savedPlace.address }</p>
-    <div>
-      <a className='saved-place-entry-link' href={'//www.images.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address + '&tbm=isch'}
-      target='_blank'>View Images</a>
-      <span className='place-entry-link-divider'>&middot;</span>
-      <a className='saved-place-entry-link' href={'//www.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address}
-      target='_blank'>Find on Google</a>
-    </div>
-  </div>
-);
+// var SavedPlaceEntry = (props) => (
+//   <div className='saved-place-entry animated fadeIn'>
+//     <p className='saved-place-name'>{ props.savedPlace.name }</p>
+//     <p className='saved-place-address'>{ props.savedPlace.address }</p>
+//     <div>
+//       <a className='saved-place-entry-link' href={'//www.images.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address + '&tbm=isch'}
+//       target='_blank'>View Images</a>
+
+//       <span className='place-entry-link-divider'>&middot;</span>
+//       <a className='saved-place-entry-link' href={'//www.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address}
+//       target='_blank'>Find on Google</a>
+
+//       <span className='place-entry-link-divider'>&middot;</span>
+//     </div>
+//   </div>
+// );
+
+import React, { Component } from 'react'
+
+class SavedPlaceEntry extends Component {
+
+  handleComplete() {
+    // this.props.actions.completeTodo(this.props.todo.id)
+  }
+
+  handleDelete() {
+    // this.props.actions.deleteTodo(this.props.todo.id)
+  }
+
+  render() {
+    return (
+      <div className='saved-place-entry animated fadeIn'>
+        <p className='saved-place-name'>{ this.props.savedPlace.name }</p>
+        <p className='saved-place-address'>{ this.props.savedPlace.address }</p>
+        <div>
+          <a className='saved-place-entry-link' href={'//www.images.google.com/search?q=' + this.props.savedPlace.name + ' ' + this.props.savedPlace.address + '&tbm=isch'}
+          target='_blank'>View Images</a>
+
+          <span className='place-entry-link-divider'>&middot;</span>
+          <a className='saved-place-entry-link' href={'//www.google.com/search?q=' + this.props.savedPlace.name + ' ' + this.props.savedPlace.address}
+          target='_blank'>Find on Google</a>
+
+          <span className='place-entry-link-divider'>&middot;</span>
+        </div>
+      </div>
+    )
+  }
+
+}
 
 export default SavedPlaceEntry;
