64b04033b29d0523ce3d840bccb8802705c14cae
Jonathen Chen <jonathen0915@gmail.com>
2016-04-21 20:01:17 -0700

    Update heart and react component to change style on favorite

--- client/components/PlaceEntry.js
+++ client/components/PlaceEntry.js
@@ -5,11 +5,14 @@ class PlaceEntry extends Component {
   constructor(props) {
     super(props);
     this.handleClick.bind(this);
+    this.state = {class: 'heart-path'};
   }
 
   handleClick(e) {
     e.preventDefault;
     this.props.onSaveClick(this.props.place, this.props.user);
+    this.setState({class: 'permahover'});
+    console.log(this.state.class);
   }
 
   render() {
@@ -29,7 +32,7 @@ class PlaceEntry extends Component {
         </div>
         <div className='place-entry-favorite'>
         <svg viewBox="0 0 32 32" onClick={this.handleClick.bind(this)} id="heart">
-          <path id="heart-path" d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454
+          <path id={this.state.class} d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454
            C30,20.335,16,28.261,16,28.261z"/>
         </svg>
         </div>
