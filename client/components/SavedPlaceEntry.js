88378b14c127eec028b9d90fd324712e0ca8fbc6
Jonathen Chen <jonathen0915@gmail.com>
2016-04-21 20:01:17 -0700

    Update heart and react component to change style on favorite

--- client/components/SavedPlaceEntry.js
+++ client/components/SavedPlaceEntry.js
@@ -7,7 +7,7 @@ var SavedPlaceEntry = (props) => (
     <p className='saved-place-address'>{ props.savedPlace.address }</p>
     <div>
       <a className='saved-place-entry-link' href={'//www.images.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address + '&tbm=isch'}
-      target='_blank'>More Vies</a>
+      target='_blank'>More Views</a>
       <span className='place-entry-link-divider'>&middot;</span>
       <a className='saved-place-entry-link' href={'//www.google.com/search?q=' + props.savedPlace.name + ' ' + props.savedPlace.address}
       target='_blank'>Find on Google</a>
