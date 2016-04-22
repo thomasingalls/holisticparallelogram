536f0fab664d2f5c8c64a7c42d97c99e57d6c077
Haoming Huang <Hao@Haomings-MacBook-Pro-2.local>
2016-04-21 18:58:41 -0700

    Clean up zombie code

--- client/reducers/savedPlaces.js
+++ client/reducers/savedPlaces.js
@@ -2,20 +2,6 @@ import * as types from '../constants/ActionTypes.js';
 
 const initialState = [];
 
-// var color = 'red';
-// switch(color){  
-//   case 'red':         
-//     console.log("Red's a cool color!");       
-//     break;        
-//   case 'blue':          
-//     console.log("Blue's good too");       
-//     break;        
-//   default:          
-//     console.log("Please choose red or blue");       
-//     break;        
-// }
-
-
 export default function places (state = initialState, action) {
   console.log('Action: ', action);
   console.log('State: ', state);
@@ -29,18 +15,13 @@ export default function places (state = initialState, action) {
       }
       return state.concat(action.place);
     case types.DELETE_PLACE:
-      // return Object.assign({}, state, {
-      //   places: state.savedPlaces.filter((placeId) => { //not sure if this works
-      //     return place.googlePlaceId !== action.savedPlace.googlePlaceId;
-      //   })
-      // })
       var newState = state.slice();
       for (var i = 0; i < newState.length; i++) {
         if (newState[i].googlePlaceId === action.placeId) {
           newState.splice(i, 1);
         }
       }
-      console.log(newState);
+      // console.log(newState);
       return newState;
     default:
       return state;
