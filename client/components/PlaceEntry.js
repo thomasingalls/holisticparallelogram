dc477e73cab1d6e24df8c86e177a71ceddbdf56a
Jonathen Chen <jonathen0915@gmail.com>
2016-04-21 19:08:54 -0700

    (style) heart icon now changes on hover

--- client/components/PlaceEntry.js
+++ client/components/PlaceEntry.js
@@ -28,7 +28,10 @@ class PlaceEntry extends Component {
             </div>
         </div>
         <div className='place-entry-favorite'>
-          <span onClick={this.handleClick.bind(this)} className='icon-heart' aria-hidden='true'></span>
+        <svg viewBox="0 0 32 32" onClick={this.handleClick.bind(this)} id="heart">
+          <path id="heart-path" d="M16,28.261c0,0-14-7.926-14-17.046c0-9.356,13.159-10.399,14-0.454c1.011-9.938,14-8.903,14,0.454
+           C30,20.335,16,28.261,16,28.261z"/>
+        </svg>
         </div>
       </div>
     );
